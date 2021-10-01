c16e358588c5e00a5507fc3833da6a9038f056a9
Rachel Cabot <rachel@bath.edu>
2021-10-02 00:52:01 +0100

    SUBMARINE COCKPIT RENDERS OVER THE SCREEN

--- Assets/SubmarineController.cs
+++ Assets/SubmarineController.cs
@@ -8,6 +8,8 @@ public class SubmarineController : MonoBehaviour
 {
     public float acceleration = 7.5f;
     public Camera playerCamera;
+    public Transform submarineCockpit;
+    public float baseLookSpeed = 2.0f;
     public float maxLookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
 
@@ -17,10 +19,13 @@ public class SubmarineController : MonoBehaviour
     public AnimationCurve frictionCurve;
 
     public float baseFriction = 0.1f;
+    public float lookSmoothTime;
 
     CharacterController characterController;
-    Vector2 currentLookSpeed;
+    Vector2 targetLookRotation;
+    Vector2 currentLookRotation;
     Vector3 currentSpeed = Vector3.zero;
+    Vector2 currentLookVelocity;
 
     void Start()
     {
@@ -39,8 +44,8 @@ public class SubmarineController : MonoBehaviour
 
     private void UpdateMovement()
     {
-        Vector3 forward = playerCamera.transform.forward;
-        Vector3 right = playerCamera.transform.right;
+        Vector3 forward = submarineCockpit.transform.forward;
+        Vector3 right = submarineCockpit.transform.right;
         float accelX = acceleration * Input.GetAxis("Vertical");
         float accelY = acceleration * Input.GetAxis("Horizontal");
 
@@ -72,11 +77,13 @@ public class SubmarineController : MonoBehaviour
     private void UpdateLookDirection()
     {
         // Player and Camera rotation
-        currentLookSpeed.x += -Input.GetAxis("Mouse Y") * maxLookSpeed;
-        currentLookSpeed.x = Mathf.Clamp(currentLookSpeed.x, -lookXLimit, lookXLimit);
-        currentLookSpeed.y = Input.GetAxis("Mouse X") * maxLookSpeed;
+        targetLookRotation.x += -Input.GetAxis("Mouse Y") * baseLookSpeed;
+        targetLookRotation.x = Mathf.Clamp(targetLookRotation.x, -lookXLimit, lookXLimit);
+        targetLookRotation.y += Input.GetAxis("Mouse X") * baseLookSpeed;
 
-        playerCamera.transform.localRotation = Quaternion.Euler(currentLookSpeed.x, 0, 0);
-        transform.rotation *= Quaternion.Euler(0, currentLookSpeed.y, 0);
+        currentLookRotation = Vector2.SmoothDamp(currentLookRotation, targetLookRotation, ref currentLookVelocity, lookSmoothTime,maxLookSpeed);
+        playerCamera.transform.rotation = Quaternion.Euler(targetLookRotation.x, targetLookRotation.y, 0);
+        submarineCockpit.localRotation = Quaternion.Euler(currentLookRotation.x, currentLookRotation.y, 0);
+        //transform.rotation *= Quaternion.Euler(0, currentLookSpeed.y, 0);
     }
 }
