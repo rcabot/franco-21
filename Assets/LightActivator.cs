565fa55ea2c9d47ec06ccd7ec989771934279c91
Rachel Cabot <rachel@bath.edu>
2021-10-02 01:53:41 +0100

    lightswitch!

--- Assets/LightActivator.cs
+++ Assets/LightActivator.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class LightActivator : MonoBehaviour
 {
-
+    public string ButtonName = "Fire2";
+    public Light light;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown(ButtonName))
+        {
+            light.enabled = !light.enabled;
+        }
     }
 }
