217ea72e10cb163b4a0a9d3fab82cc1130b0dde4
Rachel Cabot <rachel@bath.edu>
2021-10-02 12:00:57 +0100

    added tractor beam behaviour and more collctables

--- Assets/TractorBeam.cs
+++ Assets/TractorBeam.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class TractorBeam : MonoBehaviour
 {
-
+    [SerializeField] private Transform m_pullTo;
+    [SerializeField] private float m_pullForce=10.0f;
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Collectable"))
         {
-            Debug.Log("collectable in tractor beam!");
+            //Debug.Log("collectable in tractor beam!");
+            var pullDir = (m_pullTo.position - other.transform.position).normalized;
+            other.GetComponent<Rigidbody>().AddForce(pullDir * m_pullForce);
         }
 
     }
