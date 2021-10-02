6dd92950568120fb13831259adfd640d976f5dc1
Peter Ellsum <cfehunter@gmail.com>
2021-10-02 09:43:56 +0100

    Fix warnings in light activator. Hidden inherited member

--- Assets/LightActivator.cs
+++ Assets/LightActivator.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class LightActivator : MonoBehaviour
 {
-    public string ButtonName = "Fire2";
-    public Light light;
+    public string m_ButtonName = "Fire2";
+    public Light m_Light;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(ButtonName))
+        if (Input.GetButtonDown(m_ButtonName))
         {
-            light.enabled = !light.enabled;
+            m_Light.enabled = !m_Light.enabled;
         }
     }
 }
