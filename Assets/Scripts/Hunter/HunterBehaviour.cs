31bc9321eb6b39eb69e5c698ca652336e888d2d1
Peter Ellsum <cfehunter@gmail.com>
2021-10-07 22:49:05 +0100

    Fixed release build

--- Assets/Scripts/Hunter/HunterBehaviour.cs
+++ Assets/Scripts/Hunter/HunterBehaviour.cs
@@ -836,10 +836,12 @@ public partial class HunterBehaviour : MonoBehaviour
     [Conditional("UNITY_EDITOR")]
     private void LogHunterMessage(string content)
     {
+#if UNITY_EDITOR
         if (EnableLogging)
         {
             Debug.Log(content, this);
         }
+#endif
     }
 
     #endregion
