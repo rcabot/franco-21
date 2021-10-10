eaa8a293d313aa76ab6933957502969299e24fd4
Rachel Cabot <rachel@bath.edu>
2021-10-10 18:00:06 +0100

    changed to new input system

--- Assets/ControlsOverlay.cs
+++ Assets/ControlsOverlay.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ControlsOverlay : MonoBehaviour
 {
     public bool Active;
     public GameObject Overlay;
+    [SerializeField] private InputActionReference viewControlsAction;
     // Start is called before the first frame update
     void Start()
     {
         Overlay.SetActive(false);
+        viewControlsAction.action.performed += cb =>
+        {
+            Active = !Active;
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Quit"))
-        {
-            Active = !Active;
-        }
         Overlay.SetActive(Active);
     }
 }
